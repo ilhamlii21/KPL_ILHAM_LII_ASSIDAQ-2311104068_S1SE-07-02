daa039b7662c3990f5a15a7b1d8e3f647f6a56f1
williamJmoriarty <140976914+ilhamlii21@users.noreply.github.com>
2025-06-03 14:47:36 +0700

    Add files via upload

---
+++ 05_Generics/JM/JMMODUL5/JMMODUL5/Program.cs
@@ -0,0 +1,61 @@
+﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+using JMMODUL5;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string NIM = "2311104068";
+        int digit1 = int.Parse(NIM.Substring(0, 2));
+        int digit2 = int.Parse(NIM.Substring(2, 2));
+        int digit3 = int.Parse(NIM.Substring(4, 2));
+
+        char digitTerakhir = NIM[^1];
+        Penjumlahan penjumlahan = new Penjumlahan();
+        SimpleDataBase<int> database = new SimpleDataBase<int>();
+
+        if (digitTerakhir == '1' || digitTerakhir == '2')
+        {
+            float angka1 = digit1;
+            float angka2 = digit2;
+            float angka3 = digit3;
+            var hasil = penjumlahan.JumlahTigaAngka(angka1, angka2, angka3);
+            Console.WriteLine($"Hasil penjumlahan (float): {hasil}");
+        }
+        else if (digitTerakhir == '3' || digitTerakhir == '4' || digitTerakhir == '5')
+        {
+            double angka1 = digit1;
+            double angka2 = digit2;
+            double angka3 = digit3;
+            var hasil = penjumlahan.JumlahTigaAngka(angka1, angka2, angka3);
+            Console.WriteLine($"Hasil penjumlahan (double): {hasil}");
+        }
+        else if (digitTerakhir == '6' || digitTerakhir == '7' || digitTerakhir == '8')
+        {
+            int angka1 = digit1;
+            int angka2 = digit2;
+            int angka3 = digit3;
+            var hasil = penjumlahan.JumlahTigaAngka(angka1, angka2, angka3);
+            Console.WriteLine($"Hasil penjumlahan (int): {hasil}");
+        }
+        else if (digitTerakhir == '9' || digitTerakhir == '0')
+        {
+            long angka1 = digit1;
+            long angka2 = digit2;
+            long angka3 = digit3;
+            var hasil = penjumlahan.JumlahTigaAngka(angka1, angka2, angka3);
+            Console.WriteLine($"Hasil penjumlahan (long): {hasil}");
+        }
+
+        Console.WriteLine();
+
+        database.AddNewData(12);
+        database.AddNewData(34);
+        database.AddNewData(56);
+
+        database.PrintAllData();
+
+        Console.WriteLine();
+    }
+}
---
+++ 05_Generics/JM/JMMODUL5/JMMODUL5/SimpleDatabase.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JMMODUL5
+{
+    public class SimpleDataBase<T>
+    {
+        private List<T> storedData;
+        private List<DateTime> inputDates;
+
+
+        public SimpleDataBase()
+        {
+            storedData = new List<T>();
+            inputDates = new List<DateTime>();
+        }
+
+        public void AddNewData(T data)
+        {
+            storedData.Add(data);
+            inputDates.Add(DateTime.UtcNow);
+        }
+
+        public void PrintAllData()
+        {
+            for (int i = 0; i < storedData.Count; i++)
+            {
+                Console.WriteLine($"Data {i + 1} berisi: {storedData[i]}, yang disimpan pada waktu UTC: {inputDates[i]}");
+            }
+        }
+    }
+}
