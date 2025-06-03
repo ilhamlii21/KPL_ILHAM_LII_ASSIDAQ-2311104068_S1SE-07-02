188914aee8f9eb98cd3d37a8412ef5ffb8d5a830
williamJmoriarty <140976914+ilhamlii21@users.noreply.github.com>
2025-06-03 18:08:20 +0700

    Add files via upload

---
+++ 06_Design_by_Contract_dan_Defensive_Programming/TP/TPMODUL6/TPMODUL6/Program.cs
@@ -0,0 +1,23 @@
+﻿using TPMODUL6;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract oleh ilham Lii ");
+            video.IncreasePlayCount(5000000);
+            video.PrintVideoDetails();
+
+            for (int i = 0; i < 3; i++)
+            {
+                video.IncreasePlayCount(250000000);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
---
+++ 06_Design_by_Contract_dan_Defensive_Programming/TP/TPMODUL6/TPMODUL6/SayaTubeVideo.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPMODUL6
+{
+    public class SayaTubeVideo
+    {
+        private int id;
+        private string title;
+        private int playCount;
+
+        public SayaTubeVideo(string title)
+        {
+            if (string.IsNullOrEmpty(title) || title.Length > 100)
+            {
+                throw new ArgumentException("Judul video tidak boleh null dan panjangnya maksimal 100 karakter.");
+            }
+
+            Random random = new Random();
+            this.id = random.Next(10000, 99999);
+            this.title = title;
+            this.playCount = 0;
+        }
+
+        public void IncreasePlayCount(int count)
+        {
+            if (count < 0 || count > 10000000)
+            {
+                throw new ArgumentException("Penambahan play count maksimal 10.000.000 per panggilan.");
+            }
+
+            try
+            {
+                checked
+                {
+                    this.playCount += count;
+                }
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Overflow terjadi pada penambahan play count.");
+            }
+        }
+
+        public void PrintVideoDetails()
+        {
+            Console.WriteLine($"ID: {this.id}");
+            Console.WriteLine($"Title: {this.title}");
+            Console.WriteLine($"Play Count: {this.playCount}");
+        }
+    }
+}
