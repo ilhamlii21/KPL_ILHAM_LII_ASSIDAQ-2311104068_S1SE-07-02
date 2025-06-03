3f73f1a6246ada2a169851b8f3473dc74eca5884
williamJmoriarty <140976914+ilhamlii21@users.noreply.github.com>
2025-06-03 14:23:50 +0700

    Add files via upload

---
+++ 05_Generics/TP/MODUL5/MODUL5/HaloGeneric.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MODUL5
+{
+    public class HaloGeneric
+    {
+        public void SapaUser<T>(T input)
+        {
+            Console.WriteLine($"Hai User {input}");
+        }
+    }
+}
---
+++ 05_Generics/TP/MODUL5/MODUL5/Program.cs
@@ -0,0 +1,21 @@
+﻿using MODUL5;
+using System;
+namespace tpmodul5_2311104068;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Console.Write("Masukkan nama user: ");
+        string inputUser = Console.ReadLine();
+
+        HaloGeneric halo = new HaloGeneric();
+        halo.SapaUser(inputUser);
+
+        Console.WriteLine();
+
+        DataGeneric<string> data = new DataGeneric<string>("Namikaze Ilham");
+        data.PrintData();
+    }
+}
