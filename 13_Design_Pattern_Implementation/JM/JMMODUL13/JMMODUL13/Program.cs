181813ed2b1ca151ebe02d962e8ca27835252ce1
williamJmoriarty <140976914+ilhamlii21@users.noreply.github.com>
2025-06-05 01:37:27 +0700

    Add files via upload

---
+++ 13_Design_Pattern_Implementation/JM/JMMODUL13/JMMODUL13/Program.cs
@@ -0,0 +1,24 @@
+﻿using JMMODUL13;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var data1 = PusatDataSingleton.GetDataSingleton();
+        var data2 = PusatDataSingleton.GetDataSingleton();
+
+        data1.AddSebuahData("Ilham");
+        data1.AddSebuahData("Christoper");
+        data1.AddSebuahData("William");
+
+        Console.WriteLine("Data di data2:");
+        data2.PrintSemuaData();
+
+        data2.HapusSebuahData(2);
+
+        Console.WriteLine("\nSetelah penghapusan, data di data1:");
+        data1.PrintSemuaData();
+
+        Console.WriteLine($"\nJumlah data di data1: {data1.GetSemuaData().Count}");
+        Console.WriteLine($"Jumlah data di data2: {data2.GetSemuaData().Count}");
+    }
+}
---
+++ 13_Design_Pattern_Implementation/JM/JMMODUL13/JMMODUL13/PusatDataSingleton.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JMMODUL13;
+public class PusatDataSingleton
+{
+    private static PusatDataSingleton _instance;
+    public List<string> DataTersimpan { get; private set; }
+
+    private PusatDataSingleton()
+    {
+        DataTersimpan = new List<string>();
+    }
+
+    public static PusatDataSingleton GetDataSingleton()
+    {
+        if (_instance == null)
+        {
+            _instance = new PusatDataSingleton();
+        }
+        return _instance;
+    }
+
+    public List<string> GetSemuaData()
+    {
+        return DataTersimpan;
+    }
+
+    public void PrintSemuaData()
+    {
+        foreach (string data in DataTersimpan)
+        {
+            Console.WriteLine(data);
+        }
+    }
+
+    public void AddSebuahData(string input)
+    {
+        DataTersimpan.Add(input);
+    }
+
+    public void HapusSebuahData(int index)
+    {
+        if (index >= 0 && index < DataTersimpan.Count)
+        {
+            DataTersimpan.RemoveAt(index);
+        }
+    }
+}
