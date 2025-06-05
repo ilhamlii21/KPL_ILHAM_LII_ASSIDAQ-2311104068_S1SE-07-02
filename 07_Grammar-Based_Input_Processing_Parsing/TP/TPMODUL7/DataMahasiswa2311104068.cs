bd6db5f0288e8631a8ec1031e47061b5c4fbbd71
williamJmoriarty <140976914+ilhamlii21@users.noreply.github.com>
2025-06-05 11:01:13 +0700

    Add files via upload

---
+++ 07_Grammar-Based_Input_Processing_Parsing/TP/TPMODUL7/DataMahasiswa2311104068.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+
+namespace TPMODUL7
+{
+    public class DataMahasiswa2311104068
+    {
+        public class Mahasiswa
+        {
+            public string nama { get; set; }
+            public string nim { get; set; }
+            public string fakultas { get; set; }
+        }
+
+        public static void ReadJSON()
+        {
+            string jsonString = File.ReadAllText("TPMODULL_1_7.json");
+            Mahasiswa mhs = JsonSerializer.Deserialize<Mahasiswa>(jsonString);
+
+            Console.WriteLine($"Nama {mhs.nama} dengan nim {mhs.nim} dari fakultas {mhs.fakultas}");
+        }
+    }
+}
---
+++ 07_Grammar-Based_Input_Processing_Parsing/TP/TPMODUL7/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using TPMODUL7;
+
+namespace TPMODUL7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            KuliahMahasiswa2311104068.ReadJSON();
+            DataMahasiswa2311104068.ReadJSON();
+        }
+    }
+}
