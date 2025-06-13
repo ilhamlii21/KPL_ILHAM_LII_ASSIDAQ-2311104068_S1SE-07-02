f243c6a24ce73439f641e1a7e0643096443c1bbe
williamJmoriarty <140976914+ilhamlii21@users.noreply.github.com>
2025-06-13 23:26:50 +0700

    Add files via upload

---
+++ 07_Grammar-Based_Input_Processing_Parsing/JM/JMMODUL7/JMMODUL7/DataMahasiswa2311104068.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+
+
+namespace JMMODUL7
+{
+    class DataMahasiswa2311104068
+    {
+        public string NIM { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+
+        public static void ReadJSON()
+        {
+            // Path ke file JSON
+            string filePath = "JURNAL7_1.json";
+
+            // Membaca file JSON dan mendeserialize ke objek
+            string jsonData = File.ReadAllText(filePath);
+            DataMahasiswa2311104068 mahasiswa = JsonConvert.DeserializeObject<DataMahasiswa2311104068>(jsonData);
+
+            // Print hasil deserialisasi
+            Console.WriteLine($"NIM: {mahasiswa.NIM}");
+            Console.WriteLine($"First Name: {mahasiswa.FirstName}");
+            Console.WriteLine($"Last Name: {mahasiswa.LastName}");
+            Console.WriteLine($"Age: {mahasiswa.Age}");
+            Console.WriteLine($"Gender: {mahasiswa.Gender}");
+        }
+    }
+}
---
+++ 07_Grammar-Based_Input_Processing_Parsing/JM/JMMODUL7/JMMODUL7/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace JMMODUL7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataMahasiswa2311104068 mahasiswa = new DataMahasiswa2311104068();
+
+            DataMahasiswa2311104068.ReadJSON(); 
+
+        }
+    }
+}
